625f12d44b8dafbbbcac80192e39eefa165754d7
nataliasuda <natalia.suda7@wp.pl>
2025-06-17 02:19:47 +0200

    feat: add Splash screen with navigation to GetStarted screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hangmatch/screens/get_started_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (ctx) => const GetStartedScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset('assets/images/logo.svg', width: 140, height: 199),
+            const SizedBox(height: 36),
+            Text(
+              "HANGMATCH",
+              style: TextStyle(
+                color: Color(0xFF884EE9),
+                fontWeight: FontWeight.w600,
+                fontSize: 40,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
