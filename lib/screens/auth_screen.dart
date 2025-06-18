66abd8c67999785d86469b83407996039206a293
nataliasuda <natalia.suda7@wp.pl>
2025-06-18 22:36:36 +0200

    feat: support for sign in/sign up view switching

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -13,79 +13,118 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  bool isSignIn = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 68),
-              child: SvgPicture.asset(
-                'assets/images/logo.svg',
-                width: 98,
-                height: 139,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 68),
+                child: SvgPicture.asset(
+                  'assets/images/logo.svg',
+                  width: 98,
+                  height: 139,
+                ),
               ),
-            ),
-            SizedBox(height: 32),
-            Text(
-              'HANGMATCH',
-              style: TextStyle(
-                fontSize: 30,
-                color: Color(0xFF884EE9),
-                fontWeight: FontWeight.w500,
+              SizedBox(height: 32),
+              Text(
+                'HANGMATCH',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color(0xFF884EE9),
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            SizedBox(height: 81),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'SIGN IN',
-                    style: TextStyle(
-                      fontSize: 23,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
+              SizedBox(height: 81),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isSignIn = true;
+                      });
+                    },
+                    child: Text(
+                      'SIGN IN',
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        decoration:
+                            !isSignIn
+                                ? TextDecoration.none
+                                : TextDecoration.underline,
+                        decorationThickness: 3,
+                        decorationColor: Color(0xFF884EE9),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(width: 66),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'SIGN UP',
-                    style: TextStyle(
-                      fontSize: 23,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
+                  SizedBox(width: 66),
+
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isSignIn = false;
+                      });
+                    },
+                    child: Text(
+                      'SIGN UP',
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        decoration:
+                            isSignIn
+                                ? TextDecoration.none
+                                : TextDecoration.underline,
+                        decorationThickness: 3,
+                        decorationColor: Color(0xFF884EE9),
+                      ),
                     ),
                   ),
+                ],
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: isSignIn ? 30 : 20,
                 ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 65),
-              child: Form(
-                child: Column(
-                  children: [
-                    FormComponent(text: 'E-mail'),
-                    SizedBox(height: 42),
-                    FormComponent(text: 'Password', isPassword: true),
-                  ],
+                child: Form(
+                  child: Column(
+                    children: [
+                      if (!isSignIn) FormComponent(text: 'Name'),
+                      if (!isSignIn) SizedBox(height: 20),
+                      FormComponent(text: 'E-mail'),
+                      if (!isSignIn) SizedBox(height: 20),
+                      if (isSignIn) SizedBox(height: 40),
+                      FormComponent(text: 'Password', isPassword: true),
+                      if (!isSignIn) SizedBox(height: 20),
+                      if (!isSignIn)
+                        FormComponent(
+                          text: 'Repeat password',
+                          isPassword: true,
+                        ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 24),
-            GradientButton(
-              text: 'SIGN IN',
-              onPressed: () {},
-              width: 351,
-              height: 63,
-            ),
-          ],
+
+              SizedBox(height: 24),
+              GradientButton(
+                text: isSignIn ? 'SIGN IN' : 'SIGN UP',
+                onPressed: () {},
+                width: 351,
+                height: 63,
+              ),
+            ],
+          ),
         ),
       ),
     );
