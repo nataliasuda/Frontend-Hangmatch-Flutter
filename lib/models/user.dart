9908b6b32d78160cde2bbeaf2cac5bc338ee68c0
nataliasuda <natalia.suda7@wp.pl>
2025-06-23 23:31:32 +0200

    feat: add login model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -25,3 +25,14 @@ class Register {
     required this.repeatPassword,
   });
 }
+class Login {
+  final String email;
+  final String password;
+
+  Login({
+
+    required this.email,
+    required this.password,
+  });
+
+}
