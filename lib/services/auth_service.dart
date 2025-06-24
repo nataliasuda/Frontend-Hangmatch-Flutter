3c4c82ff08d5092d20795a84c713a82013db218f
nataliasuda <natalia.suda7@wp.pl>
2025-06-24 21:36:50 +0200

    feat: error handling with SnackBar messages from backend

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -11,42 +11,54 @@ class UserService {
   Future<void> register(BuildContext context, Register register) async {
     final url = Uri.parse('$baseUrl/register');
 
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'name': register.name,
-        'email': register.email,
-        'password': register.password,
-        'repeated_password': register.repeatPassword,
-      }),
-    );
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'name': register.name,
+          'email': register.email,
+          'password': register.password,
+          'repeated_password': register.repeatPassword,
+        }),
+      );
+      final responseData = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        _showSnackBar(context, responseData['message'], true);
+      } else {
+        _showSnackBar(context, responseData['detail'], false);
+      }
+    } catch (e) {
+      _showSnackBar(context, 'An error occurred: $e', false);
+    }
   }
 
   Future<void> login(BuildContext context, Login login) async {
     final url = Uri.parse('$baseUrl/login');
 
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'email': login.email, 'password': login.password}),
-    );
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final token = data['access_token'];
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'email': login.email, 'password': login.password}),
+      );
+      final responseData = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        final token = responseData['access_token'];
 
-      if (token != null) {
-        await TokenService().saveToken(token);
-        await getCurrentUser(context);
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const HomeScreen()),
-        );
+        if (token != null) {
+          await TokenService().saveToken(token);
+          await getCurrentUser(context);
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
+          );
+        }
+      } else {
+        _showSnackBar(context, responseData['detail'], false);
       }
+    } catch (e) {
+      _showSnackBar(context, 'An error occurred: $e', false);
     }
   }
 
@@ -57,7 +69,7 @@ class UserService {
       return;
     }
 
-    final url = Uri.parse('http://10.0.2.2:8000/users/me');
+    final url = Uri.parse('$baseUrl/users/me');
 
     final response = await http.get(
       url,
@@ -66,10 +78,20 @@ class UserService {
         'Authorization': 'Bearer $token',
       },
     );
-
+    final responseData = jsonDecode(response.body);
     if (response.statusCode == 200) {
-      final userData = jsonDecode(response.body);
-      print('User data: $userData');
+      _showSnackBar(context, 'message: ${responseData['name']}', true);
+    } else {
+      _showSnackBar(context, responseData['detail'], false);
     }
   }
+}
+
+void _showSnackBar(BuildContext context, String message, bool success) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      backgroundColor: success ? Colors.green : Colors.red,
+      content: Text(message),
+    ),
+  );
 }
