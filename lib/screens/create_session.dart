df04b54f049db3388702f55b800ad61a94ec6598
nataliasuda <natalia.suda7@wp.pl>
2025-06-27 00:02:48 +0200

    feat: add invite friends button and SliderLocation to CreateSessionScreen

--- lib/screens/create_session.dart
+++ lib/screens/create_session.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hangmatch/widgets/form_component.dart';
 import 'package:hangmatch/widgets/gradient_button.dart';
 import 'package:hangmatch/widgets/heading.dart';
+import 'package:hangmatch/widgets/slider_location.dart';
 
 class CreateSessionScreen extends StatefulWidget {
   const CreateSessionScreen({super.key});
@@ -38,23 +39,20 @@ class _CreateSessionScreenState extends State<CreateSessionScreen> {
         foregroundColor: Colors.white,
         title: const Heading(text: 'Create session', fontSize: 25),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 50),
-            child: Form(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Form(
               key: _form,
-            
               child: FormComponent(
                 text: 'Session name',
-              
                 controller: _sessionNameController,
-              
                 validator: (value) {
                   if (value == null || value.trim().isEmpty) {
-                    return 'Please enter session name.';
+                    return 'Please enter a session name.';
                   }
-              
                   return null;
                 },
                 onSaved: (value) {
@@ -62,16 +60,45 @@ class _CreateSessionScreenState extends State<CreateSessionScreen> {
                 },
               ),
             ),
-          ),
-          GradientButton(
-            width: 351,
-            height: 63,
-            text: 'START SESSION',
-            onPressed: _submit,
-          ),
-        ],
+            SizedBox(height: 35),
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(
+                  0xFFD593F7,
+                ).withAlpha((0.75 * 255).toInt()),
+              ),
+              onPressed: () {},
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.person_add, color: Colors.white, size: 24),
+                  SizedBox(width: 8),
+                  Text(
+                    'Invite friends',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 50),
+            Text(
+              'Distance [km]',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+
+            SliderLocation(),
+            const SizedBox(height: 80),
+            GradientButton(
+              width: 351,
+              height: 63,
+              text: 'START SESSION',
+              onPressed: _submit,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
