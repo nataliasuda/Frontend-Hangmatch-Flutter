b2aa1bbca981e9682bb229379819c38408b23817
nataliasuda <natalia.suda7@wp.pl>
2025-06-26 13:11:57 +0200

    feat: implement HomeScreen layout with welcome message and NewSessionButton and other headings

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hangmatch/widgets/heading.dart';
+
+import 'package:hangmatch/widgets/home/new_session_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 75),
+                  child: Heading(text: 'Welcome Back!', fontSize: 30),
+                ),
+                SizedBox(width: 33),
+                Padding(
+                  padding: const EdgeInsets.only(top: 48),
+                  child: SvgPicture.asset(
+                    'assets/images/logo.svg',
+                    width: 56,
+                    height: 77,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 11),
+            NewSessionButton(),
+
+            Heading(text: 'Last session'),
+            SizedBox(height: 22),
+
+            SizedBox(height: 22),
+            Heading(text: 'Upcoming events'),
+            SizedBox(height: 22),
+
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
   }
 }
