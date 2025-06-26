f5650e89a6b1314aecd1976e2add9beb27f72c28
nataliasuda <natalia.suda7@wp.pl>
2025-06-26 23:56:24 +0200

    feat: implement CreateSessionScreen with form and button for session creation

--- lib/screens/create_session.dart
+++ lib/screens/create_session.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hangmatch/widgets/form_component.dart';
+import 'package:hangmatch/widgets/gradient_button.dart';
 import 'package:hangmatch/widgets/heading.dart';
 
 class CreateSessionScreen extends StatefulWidget {
@@ -9,6 +11,25 @@ class CreateSessionScreen extends StatefulWidget {
 }
 
 class _CreateSessionScreenState extends State<CreateSessionScreen> {
+  final _form = GlobalKey<FormState>();
+  final _sessionNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _sessionNameController.dispose();
+
+    super.dispose();
+  }
+
+  void _submit() async {
+    final isValid = _form.currentState!.validate();
+
+    if (!isValid) {
+      return;
+    }
+    _form.currentState?.save();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +38,40 @@ class _CreateSessionScreenState extends State<CreateSessionScreen> {
         foregroundColor: Colors.white,
         title: const Heading(text: 'Create session', fontSize: 25),
       ),
-      body: Column(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 50),
+            child: Form(
+              key: _form,
+            
+              child: FormComponent(
+                text: 'Session name',
+              
+                controller: _sessionNameController,
+              
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Please enter session name.';
+                  }
+              
+                  return null;
+                },
+                onSaved: (value) {
+                  _sessionNameController.text = value!;
+                },
+              ),
+            ),
+          ),
+          GradientButton(
+            width: 351,
+            height: 63,
+            text: 'START SESSION',
+            onPressed: _submit,
+          ),
+        ],
+      ),
     );
   }
 }
+
