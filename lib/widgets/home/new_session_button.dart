24edca4ddeeb3cecbf94a5e61381d2cf5cdf58f7
nataliasuda <natalia.suda7@wp.pl>
2025-06-28 01:32:13 +0200

    feat: add onSessionCreated callback to NewSessionButton

--- lib/widgets/home/new_session_button.dart
+++ lib/widgets/home/new_session_button.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:hangmatch/screens/create_session.dart';
 
 class NewSessionButton extends StatelessWidget {
-  const NewSessionButton({super.key});
+  final VoidCallback onSessionCreated;
+
+  const NewSessionButton({super.key, required this.onSessionCreated});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(31),
       child: ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).push(
+        onPressed: () async {
+          final result = await Navigator.of(context).push(
             MaterialPageRoute(
               builder: (context) => const CreateSessionScreen(),
             ),
           );
+          if (result == true) {
+            onSessionCreated();
+          }
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: Color(0xFFD593F7).withAlpha((0.75 * 255).toInt()),
