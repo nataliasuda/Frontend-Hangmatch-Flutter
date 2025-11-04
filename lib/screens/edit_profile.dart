5c6e2c40d6638f4d56ff2574de41383400be7508
nataliasuda <natalia.suda7@wp.pl>
2025-11-04 23:22:20 +0100

    feat: create basic edit profile screen structure

--- lib/screens/edit_profile.dart
+++ lib/screens/edit_profile.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
 
-class EditProfileScreen extends StatelessWidget {
+class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({super.key});
+
   @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text('Edit profile'));
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  final TextEditingController nameController = TextEditingController(
+    text: 'Natalia',
+  );
+  final TextEditingController emailController = TextEditingController(
+    text: 'natalia@example.com',
+  );
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          'Edit profile',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white70),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        child: ListView(children: [const SizedBox(height: 10)]),
+      ),
+    );
+  }
 }
