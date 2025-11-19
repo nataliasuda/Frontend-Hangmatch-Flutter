dfc8f1a5aa381c7c73c2919ecfd3ca8ac6c3df7a
nataliasuda <natalia.suda7@wp.pl>
2025-11-19 03:46:20 +0100

    feat: implement notifcation widget into HomeScreen, add Padding

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,7 @@ import 'package:hangmatch/services/session_service.dart';
 import 'package:hangmatch/widgets/heading.dart';
 import 'package:hangmatch/widgets/home/last_session_card.dart';
 import 'package:hangmatch/widgets/home/new_session_button.dart';
+import 'package:hangmatch/widgets/home/notification_overlay.dart';
 import 'package:hangmatch/widgets/home/upcoming_event_card.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,6 +17,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Session> sessions = [];
+  final _notificationController = NotificationOverlayController();
 
   @override
   void initState() {
@@ -33,95 +35,120 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 15, left: 20),
-              child: Container(
-                width: 46,
-                height: 46,
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.1),
-                  borderRadius: BorderRadius.circular(22),
-                ),
-                child: Icon(
-                  Icons.notifications_sharp,
-                  size: 26,
-                  color: Color(0xFFD593F7),
-                ),
-              ),
-            ),
-
-            Row(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top: 27),
-                  child: Heading(text: 'Welcome Back!', fontSize: 30),
-                ),
-                SizedBox(width: 33),
-                Padding(
-                  padding: const EdgeInsets.only(top: 0),
-                  child: SvgPicture.asset(
-                    'assets/images/logo.svg',
-                    width: 56,
-                    height: 77,
+                  padding: const EdgeInsets.only(top: 15, left: 20),
+                  child: GestureDetector(
+                    onTap: _notificationController.toggle,
+                    child: Container(
+                      width: 46,
+                      height: 46,
+                      decoration: BoxDecoration(
+                        color:
+                            _notificationController.isVisible
+                                ? Colors.white.withOpacity(0.2)
+                                : Colors.white.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(22),
+                      ),
+                      child: Icon(
+                        Icons.notifications_sharp,
+                        size: 26,
+                        color: const Color(0xFFD593F7),
+                      ),
+                    ),
                   ),
                 ),
-              ],
-            ),
 
-            SizedBox(height: 11),
-            NewSessionButton(onSessionCreated: _fetchSessions),
-
-            Heading(text: 'Last session'),
-            SizedBox(height: 22),
-            sessions.isEmpty
-                ? Padding(
-                  padding: const EdgeInsets.only(left: 150),
-                  child: const Text('No sessions found.'),
-                )
-                : Column(
+                Row(
                   children: [
-                    if (sessions.isNotEmpty)
-                      LastSessionCard(
-                        title: sessions[0].name,
-                        onPressed: () {},
+                    const Padding(
+                      padding: EdgeInsets.only(top: 27),
+                      child: Heading(text: 'Welcome Back!', fontSize: 30),
+                    ),
+                    const SizedBox(width: 33),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 0),
+                      child: SvgPicture.asset(
+                        'assets/images/logo.svg',
+                        width: 56,
+                        height: 77,
                       ),
-                    if (sessions.length > 1) ...[
-                      const SizedBox(height: 20),
-                      LastSessionCard(
-                        title: sessions[1].name,
-                        onPressed: () {},
-                      ),
-                    ],
+                    ),
                   ],
                 ),
 
-            SizedBox(height: 22),
-            Heading(text: 'Upcoming events'),
-            SizedBox(height: 22),
-            UpcomingEventCard(
-              title: 'Juwenalia 2025',
-              date: 'May 9-10 2025',
-              onPressed: () {},
+                const SizedBox(height: 11),
+
+                NewSessionButton(onSessionCreated: _fetchSessions),
+
+                Heading(text: 'Last session'),
+
+                const SizedBox(height: 22),
+                sessions.isEmpty
+                    ? const Padding(
+                      padding: EdgeInsets.only(left: 150),
+                      child: Text('No sessions found.'),
+                    )
+                    : Column(
+                      children: [
+                        if (sessions.isNotEmpty)
+                          LastSessionCard(
+                            title: sessions[0].name,
+                            onPressed: () {},
+                          ),
+                        if (sessions.length > 1) ...[
+                          const SizedBox(height: 20),
+                          LastSessionCard(
+                            title: sessions[1].name,
+                            onPressed: () {},
+                          ),
+                        ],
+                      ],
+                    ),
+
+                const SizedBox(height: 22),
+                Heading(text: 'Upcoming events'),
+
+                const SizedBox(height: 22),
+                Column(
+                  children: [
+                    UpcomingEventCard(
+                      title: 'Juwenalia 2025',
+                      date: 'May 9-10 2025',
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 20),
+                    UpcomingEventCard(
+                      title: 'Concert',
+                      date: 'May 31 2025',
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 20),
+                    UpcomingEventCard(
+                      title: 'Fountain show',
+                      date: 'June 20 2025',
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            UpcomingEventCard(
-              title: 'Concert',
-              date: 'May 31 2025',
-              onPressed: () {},
-            ),
-            SizedBox(height: 20),
-            UpcomingEventCard(
-              title: 'Fountain show',
-              date: 'June 20 2025',
-              onPressed: () {},
-            ),
-          ],
-        ),
+          ),
+
+          NotificationOverlay(controller: _notificationController),
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _notificationController.dispose();
+    super.dispose();
   }
 }
