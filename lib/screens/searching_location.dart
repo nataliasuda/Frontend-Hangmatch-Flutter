f0e5bee9059dddb94eb9c1ab846ef7f1a4598a2d
nataliasuda <natalia.suda7@wp.pl>
2025-11-20 15:32:36 +0100

    feat: add basic SearchingLocationScreen

---
+++ lib/screens/searching_location.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class SearchingLocation extends StatelessWidget {
+  const SearchingLocation({super.key, required String sessionId});
+  @override
+  Widget build(BuildContext context) => const Center(child: Text('Searching for location...'));
+}
