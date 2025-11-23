52fd151bd321aa5b500da4b88a21e35a9cdcd910
nataliasuda <natalia.suda7@wp.pl>
2025-11-23 15:35:44 +0100

    feat: add sendVote method to EventService

--- lib/services/event_service.dart
+++ lib/services/event_service.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:hangmatch/services/token_service.dart';
+
 import '../models/event.dart';
 
 class EventService {
   final ticketmasterApiKey = dotenv.env['TICKETMASTER_API_KEY']!;
+  final TokenService _tokenService = TokenService();
+  final String baseUrl = 'http://10.0.2.2:8000';
+
 
   Future<List<Event>> fetchEvents(String city) async {
     final url = Uri.parse(
@@ -13,10 +18,30 @@ class EventService {
 
     final response = await http.get(url);
     final data = jsonDecode(response.body);
-    final events =
-        (data['_embedded']['events'] as List)
-            .map((e) => Event.fromTicketmaster(e))
-            .toList();
+
+    if (data['_embedded'] == null) return [];
+
+    final events = (data['_embedded']['events'] as List)
+        .map((e) => Event.fromTicketmaster(e))
+        .toList();
+
     return events;
+  }
+
+  Future<void> sendVote(String sessionId, String eventId, String vote) async {
+    final url = Uri.parse('$baseUrl/sessions/$sessionId/vote');
+
+    final response = await _tokenService.authorizedPost(
+      url,
+      body: {
+        "event_id": eventId,
+        "vote": vote,
+      },
+    );
+
+    if (response.statusCode != 200) {
+      print(response.body);
+      throw Exception("Failed to send vote");
+    }
   }
 }
