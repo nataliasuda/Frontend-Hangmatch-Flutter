de56b5cbb8e3e8226a33b0650942bbb3c777149a
nataliasuda <natalia.suda7@wp.pl>
2025-11-27 21:20:12 +0100

    feat: add interactive avatar upload with image picker

--- lib/widgets/profile/avatar_section.dart
+++ lib/widgets/profile/avatar_section.dart
@@ -1,41 +1,106 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:hangmatch/services/auth_service.dart';
+import 'package:image_picker/image_picker.dart';
 
-class AvatarSection extends StatelessWidget {
-  const AvatarSection({super.key});
+class AvatarSection extends StatefulWidget {
+  final String? currentAvatarUrl;
+  final Function(String?) onAvatarChanged;
+
+  const AvatarSection({
+    super.key,
+    this.currentAvatarUrl,
+    required this.onAvatarChanged,
+  });
+
+  @override
+  State<AvatarSection> createState() => _AvatarSectionState();
+}
+
+class _AvatarSectionState extends State<AvatarSection> {
+  final UserService _userService = UserService();
+  bool _isLoading = false;
+
+  Future<void> _pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 400,
+      maxHeight: 400,
+      imageQuality: 80,
+    );
+
+    if (image != null) {
+      await _uploadImage(File(image.path));
+    }
+  }
+
+  Future<void> _uploadImage(File imageFile) async {
+    setState(() => _isLoading = true);
+    try {
+      final response = await _userService.updateAvatar(imageFile);
+      widget.onAvatarChanged(response['avatar_url']);
+    } catch (e) {
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
+    return GestureDetector(
+      onTap: _isLoading ? null : _pickImage,
+      child: Column(
         children: [
-          const CircleAvatar(
-            radius: 50,
-            backgroundColor: Colors.white12,
-            child: Icon(Icons.person, color: Colors.white, size: 50),
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: const LinearGradient(
-                  colors: [Colors.purpleAccent, Colors.deepPurple],
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.purpleAccent.withOpacity(0.4),
-                    blurRadius: 8,
+          Stack(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.grey[800],
+                backgroundImage:
+                    widget.currentAvatarUrl != null
+                        ? NetworkImage(
+                          '${_userService.baseUrl}${widget.currentAvatarUrl!}',
+                        )
+                        : null,
+                child:
+                    _isLoading
+                        ? const CircularProgressIndicator(color: Colors.white)
+                        : widget.currentAvatarUrl == null
+                        ? Icon(Icons.camera_alt, size: 40, color: Colors.grey)
+                        : null,
+              ),
+              if (!_isLoading)
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: const LinearGradient(
+                        colors: [Colors.purpleAccent, Colors.deepPurple],
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.purpleAccent.withOpacity(0.4),
+                          blurRadius: 8,
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.all(6),
+                    child: const Icon(
+                      Icons.edit,
+                      size: 18,
+                      color: Colors.white,
+                    ),
                   ),
-                ],
-              ),
-              padding: const EdgeInsets.all(6),
-              child: const Icon(
-                Icons.edit,
-                size: 18,
-                color: Colors.white,
-              ),
-            ),
+                ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Tap to change avatar',
+            style: TextStyle(color: Colors.grey[400], fontSize: 12),
           ),
         ],
       ),
